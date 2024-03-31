635f1a6f9cf38085711cd05ea8fec331f7a6edf4
shenchangqing <shenqinpeng@126.com>
2024-04-01 00:02:33 +0800

    add write to file

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"flag"
-	"fmt"
+	"log"
 	"log/slog"
 	"os"
 	"os/signal"
@@ -18,6 +18,7 @@ import (
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/util/homedir"
 
 	appsv1 "k8s.io/api/apps/v1"
 )
@@ -26,6 +27,7 @@ var (
 	KUBE_USED_NS    = "kube-system|kube-public|kube-node-lease"
 	not_watched_ns  string
 	some_image_host string
+	record_log_path = homedir.HomeDir() + "/.deploy_image_recore.log"
 )
 
 type PodUpdateRecord struct {
@@ -37,7 +39,7 @@ type PodUpdateRecord struct {
 
 func main() {
 
-	flag.StringVar(&some_image_host, "image-host", "", "image host regexp math you want to watch, like '.*tecnent.cloudtr.com.*'")
+	flag.StringVar(&some_image_host, "image-host", "", "image host regexp math you want to watch, like '.*tecnent.cloudtcr.com.*'")
 	flag.StringVar(&not_watched_ns, "no-ns", KUBE_USED_NS, "ns name not watched, like:"+KUBE_USED_NS)
 	flag.Parse()
 	slog.Info("main run...")
@@ -70,59 +72,14 @@ func WatchDeploy(cs *kubernetes.Clientset, stopCh <-chan struct{}, notWatchedNs 
 
 	notWatchNsSlice := strings.Split(notWatchedNs, "|")
 
-	// ns except
+	// ns exclude
 	informerFactory := informers.NewSharedInformerFactoryWithOptions(cs, time.Second*60, informers.WithTweakListOptions(func(lo *metav1.ListOptions) {
 		lo.FieldSelector = getNotEqualFieldSelectorString(notWatchNsSlice)
 	}))
 	deployInformer := informerFactory.Apps().V1().Deployments()
 
 	deployInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
-		UpdateFunc: func(oldObj, newObj interface{}) {
-			oldDeploy, ok1 := oldObj.(*appsv1.Deployment)
-			newDeploy, ok2 := newObj.(*appsv1.Deployment)
-
-			if !(ok1 && ok2) {
-				// slog.Error("deployInformer", "msg", "deploy informer updateFunc get deploy cr error")
-				return
-			}
-
-			if oldDeploy.ResourceVersion == newDeploy.ResourceVersion {
-				return
-			}
-
-			oldPodImage := getImageTag(oldDeploy)
-			newPodImage := getImageTag(newDeploy)
-
-			// fmt.Printf("name=%s,ns=%s,%v\n", oldDeploy.Name, oldDeploy.Namespace, oldPodImage)
-
-			// first check container.name, then check image_name same
-			for ok, ov := range oldPodImage {
-				nv, found := newPodImage[ok]
-				if found {
-					oldImageTag := strings.Split(ov, ":")
-					newImageTag := strings.Split(nv, ":")
-					if len(oldImageTag) == 2 && len(newImageTag) == 2 && oldImageTag[1] != newImageTag[1] {
-						podUpdateRecord := PodUpdateRecord{
-							ImageName: oldImageTag[0],
-							OldTag:    oldImageTag[1],
-							NewTag:    newImageTag[1],
-							UpdateAt:  time.Now().Format(time.RFC3339),
-						}
-
-						b, err := json.Marshal(podUpdateRecord)
-						if err != nil {
-
-							// slog.Error("json marshal err", "msg", err)
-							return
-						}
-
-						fmt.Println(string(b))
-
-						return
-					}
-				}
-			}
-		},
+		UpdateFunc: handlerUpdate,
 	})
 	informerFactory.Start(stopCh)
 	informerFactory.WaitForCacheSync(stopCh)
@@ -132,6 +89,80 @@ func WatchDeploy(cs *kubernetes.Clientset, stopCh <-chan struct{}, notWatchedNs 
 	slog.Info("stop informerFactory...")
 	return nil
 
+}
+
+func handlerUpdate(oldObj, newObj interface{}) {
+	oldDeploy, ok1 := oldObj.(*appsv1.Deployment)
+	newDeploy, ok2 := newObj.(*appsv1.Deployment)
+
+	if !(ok1 && ok2) {
+		// slog.Error("deployInformer", "msg", "deploy informer updateFunc get deploy cr error")
+		return
+	}
+
+	if oldDeploy.ResourceVersion == newDeploy.ResourceVersion {
+		return
+	}
+
+	oldPodImage := getImageTag(oldDeploy)
+	newPodImage := getImageTag(newDeploy)
+
+	// fmt.Printf("name=%s,ns=%s,%v\n", oldDeploy.Name, oldDeploy.Namespace, oldPodImage)
+
+	// first check container.name, then check image_name same
+	for ok, ov := range oldPodImage {
+		nv, found := newPodImage[ok]
+		if found {
+			oldImageTag := strings.Split(ov, ":")
+			newImageTag := strings.Split(nv, ":")
+			if len(oldImageTag) == 2 && len(newImageTag) == 2 && oldImageTag[1] != newImageTag[1] {
+				podUpdateRecord := PodUpdateRecord{
+					ImageName: oldImageTag[0],
+					OldTag:    oldImageTag[1],
+					NewTag:    newImageTag[1],
+					UpdateAt:  time.Now().Format(time.RFC3339),
+				}
+
+				b, err := json.Marshal(podUpdateRecord)
+				if err != nil {
+
+					slog.Error("json marshal err", "msg", err)
+					return
+				}
+
+				log.Println(string(b))
+
+				// write to file
+				if err := writeToFile(b, record_log_path); err != nil {
+					slog.Error("write to file", "msg", err)
+				}
+
+				return
+			}
+		}
+	}
+}
+
+func writeToFile(src []byte, path string) error {
+
+	f, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+
+	defer f.Close()
+
+	_, err = f.Write(src)
+	if err != nil {
+		return err
+	}
+
+	_, err = f.WriteString("\n")
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func getImageTag(pod *appsv1.Deployment) map[string]string {
